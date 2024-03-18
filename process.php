44ed9f45452ada8fd3177072814bb0b535166b6a
Erick Mafra <127338733+Erick-Mafra@users.noreply.github.com>
2024-03-18 17:54:53 -0300

    Update process.php adding download functionality on the page

--- process.php
+++ process.php
@@ -1,9 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="description" content="um conversor de tipos de imagem seja de png para jpeg seja de gif para jpeg ou jpeg para webp teste e se surpeendera com a facilidade">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Images download</title>
+    <style>
+        a{
+            display: none;
+        }
+        body{
+            background-color: black;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $imgsnames= [];
     if(isset($_FILES["imagens"])){
         $typeselect = $_POST['typeselect'];
         $imgs = $_FILES["imagens"];
         for($control = 0;$control<count($imgs['name']);$control++){
             $nameoffile = md5(time());
+            //$imgsnames += $nameoffile;
+            array_push($imgsnames,$nameoffile);
             switch ($imgs['type'][$control]) {
                 case 'image/png' || 'image/x-png':
                     $tempimg = imagecreatefrompng($imgs['tmp_name'][$control]);
@@ -15,7 +35,7 @@
                 case 'image/webp':
                     $tempimg = imagecreatefromwebp($imgs['tmp_name'][$control]);
                     break;
-                case 'image/gif':
+                    case 'image/gif':
                     $tempimg = imagecreatefromgif($imgs['tmp_name'][$control]);
                     break;
                 default:
@@ -38,13 +58,24 @@
                         case 'gif':
                             imagegif($tempimg,"./images/{$nameoffile}.{$typeselect}");
                             break;
-                        default:
+                            default:
                             echo "Error";
                             break;
                     }
                 }
-        }
-        
+            }
+    for($control1 = 0;$control1<count($imgsnames);$control1++){
+        echo "<a href='./images/{$imgsnames[$control1]}.{$typeselect}' download='{$imgsnames[$control1]}.{$typeselect}'></a>";
     }
-
-?>
+    }
+    ?>
+    <script>
+            document.querySelectorAll('a').forEach((e)=>{
+                e.click()
+            })
+            setTimeout(()=>{
+                window.location.href = 'https://darkeclipse.com.br/corverterimg/'
+            },3000)
+    </script>
+</body>
+</html>
